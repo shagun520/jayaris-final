a949219ec21a48e8e2c16eaa500bcee19d00b58f
shagun verma <shagunverma@shaguns-MacBook-Air.local>
2025-07-22 17:01:22 +0530

    🔥 update: about us page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App1 from './App1';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App1 from "./App1"; // assuming App1 is your main app file
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter >
+  <BrowserRouter basename="/jayaris-final">
     <App1 />
   </BrowserRouter>
 );
