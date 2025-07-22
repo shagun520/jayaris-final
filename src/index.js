271aa1537dd7faff75bcec6521ac2a2bbb936bfa
shagun verma <shagunverma@shaguns-MacBook-Air.local>
2025-07-22 12:35:54 +0530

    responsiveness and bg in services

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App1 from './App1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/jayaris-final' : '/'}>
+  <HashRouter>
     <App1 />
-  </BrowserRouter>
+  </HashRouter>
 );
